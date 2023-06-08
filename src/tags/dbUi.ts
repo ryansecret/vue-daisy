02ae14c480f0c1f64afe721da05a622e5e0c7349
chenjingnan <chenjingnan@jd.com>
2023-06-08 18:19:03 +0800

    feat(snippets): :sparkles: dbUI 组件添加snippet & tag 提示

---
+++ src/tags/dbUi.ts
@@ -0,0 +1,18 @@
+export default {
+     "db-status": {
+        "attributes": ["status","statusText", "middleStatus","errorStatus"],
+        "description": "instance status",
+        "framework": "dbUI"
+      },
+      "db-auto-load-select": {
+        "attributes": ["v-model","valueKey","lableKey", "getList","pageSize"],
+        "description": "下拉自动加载的select",
+        "framework": "dbUI"
+      },
+      "db-date-range": {
+        "attributes": ["defaultShortcutsIndex","timeDefault","enabledInit", "needDatePicker"],
+        "description": "下拉自动加载的select",
+        "framework": "dbUI"
+      },
+      
+};
--- src/vue-tags.ts
+++ src/vue-tags.ts
@@ -3,10 +3,12 @@ import vux from './tags/vux';
 import iview2 from './tags/iview2';
 import vue from './tags/vue';
 import jelementBiz from './tags/jelement-biz';
+import dbUi  from './tags/dbUi';
 
 export default {
   ...element,
   ...jelementBiz,
+  ...dbUi,
   ...vux,
   ...iview2,
   ...vue
