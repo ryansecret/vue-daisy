650c33112ce3c38805addcfcb25e026697d07c92
chenjingnan <chenjingnan@jd.com>
2023-10-10 19:44:27 +0800

    feat(___CUSTOM___): :sparkles: 更新jelement-biz 文档

--- src/vue-attributes.ts
+++ src/vue-attributes.ts
@@ -1,12 +1,13 @@
-import vux from "./attributes/vux"
-import element from "./attributes/element"
-import iview2 from "./attributes/iview2"
-import vue from "./attributes/vue"
-
+import vux from "./attributes/vux";
+import element from "./attributes/element";
+import iview2 from "./attributes/iview2";
+import vue from "./attributes/vue";
+import jelementBiz from './attributes/jelement-biz';
 export default {
   ...element,
   ...vux,
   ...iview2,
-  ...vue
-}
+  ...vue,
+  ...jelementBiz
+};
 
